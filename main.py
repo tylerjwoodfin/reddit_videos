611c7cf6e5ea05181deccf82d7444385f31d7ac4
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-11-27 21:37:26 -0600

    Added automatic YouTube upload

--- main.py
+++ main.py
@@ -4,17 +4,32 @@ reddit_videos
 see README for important information
 """
 
+#!/usr/bin/python
+
 import os
 import math
 import textwrap
+import http.client as httplib
+import random
+import sys
+import time
 import requests
+import httplib2
 from securedata import securedata
 from PIL import Image, ImageDraw, ImageFont
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
 
 IMG_SIZE = (1920, 1080)
 IMG_MSG = "This is sample text"
 IMG_FONT = ImageFont.truetype('arial.ttf', 60)
 REDDIT = securedata.getItem("reddit")
+VIDEO_TITLE = "Old School Cool"
 
 
 def create_text_image(size, bg_color, message, font, font_color):
@@ -89,6 +104,9 @@ def create_video():
     """
     Creates a video from downloaded Reddit files, then adds audio
     """
+
+    global VIDEO_TITLE
+
     auth = requests.auth.HTTPBasicAuth(
         REDDIT['personal_script'], REDDIT['secret'])
     data = {'grant_type': 'password',
@@ -128,6 +146,8 @@ def create_video():
         if str(post['data']['url']).endswith('jpg'):
             img_count += 1
 
+            VIDEO_TITLE = post['data']['title']
+
             # create title card image
             image = create_text_image((1920, 1080), 'black',
                                       post['data']['title'], IMG_FONT, 'yellow')
@@ -157,11 +177,198 @@ def create_video():
     os.system("""ffmpeg -i ./output/noaudio.mp4 -i ./assets/beautiful_life.mp3 -map 0:v -map 1:a -c:v copy -shortest output.mp4""")
 
 
+# Explicitly tell the underlying HTTP transport library not to retry, since
+# we are handling retry logic ourselves.
+httplib2.RETRIES = 1
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
+                        httplib.IncompleteRead, httplib.ImproperConnectionState,
+                        httplib.CannotSendRequest, httplib.CannotSendHeader,
+                        httplib.ResponseNotReady, httplib.BadStatusLine)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+# The CLIENT_SECRETS variable specifies the data in securedata that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
+# the Google API Console at
+# https://console.cloud.google.com/.
+# Please ensure that you have enabled the YouTube Data API for your project.
+# For more information about using OAuth2 to access the YouTube Data API, see:
+#   https://developers.google.com/youtube/v3/guides/authentication
+# For more information about the client_secrets.json file format, see:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+
+# This OAuth 2.0 access scope allows an application to upload files to the
+# authenticated user's YouTube channel, but doesn't allow other types of access.
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# This variable defines a message to display if the CLIENT_SECRETS_FILE is
+# missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the securedata's settings.json -> reddit -> google-oath as follows:
+
+"google-oath": {
+            "web": {
+                "client_id": "<<your ID here>>",
+                "client_secret": "<<your secret here>>",
+                "redirect_uris": [],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://accounts.google.com/o/oauth2/token"
+            }
+        }
+
+For more information about the client_secrets file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+"""
+
+VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
+
+
+def get_authenticated_service(args):
+    """
+    Authorize client
+    """
+
+    flow = flow_from_clientsecrets("client-secrets.json",
+                                   scope=YOUTUBE_UPLOAD_SCOPE,
+                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+    storage = Storage(f"{sys.argv[0]}-oauth2.json")
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        credentials = run_flow(flow, storage, args)
+
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                 http=credentials.authorize(httplib2.Http()))
+
+
+def initialize_upload(youtube, options):
+    """
+    Initialize video upload
+    """
+    tags = None
+    if options.keywords:
+        tags = options.keywords.split(",")
+
+    body = dict(
+        snippet=dict(
+            title=options.title,
+            description=options.description,
+            tags=tags,
+            categoryId=options.category
+        ),
+        status=dict(
+            privacyStatus=options.privacyStatus
+        )
+    )
+
+    # Call the API's videos.insert method to create and upload the video.
+    insert_request = youtube.videos().insert(
+        part=",".join(body.keys()),
+        body=body,
+        # The chunksize parameter specifies the size of each chunk of data, in
+        # bytes, that will be uploaded at a time. Set a higher value for
+        # reliable connections as fewer chunks lead to faster uploads. Set a lower
+        # value for better recovery on less reliable connections.
+        #
+        # Setting "chunksize" equal to -1 in the code below means that the entire
+        # file will be uploaded in a single HTTP request. (If the upload fails,
+        # it will still be retried where it left off.) This is usually a best
+        # practice, but if you're using Python older than 2.6 or if you're
+        # running on App Engine, you should set the chunksize to something like
+        # 1024 * 1024 (1 megabyte).
+        media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
+    )
+
+    resumable_upload(insert_request)
+
+# This method implements an exponential backoff strategy to resume a
+# failed upload.
+
+
+def resumable_upload(insert_request):
+    """
+    Upload video
+    """
+    response = None
+    error = None
+    retry = 0
+    while response is None:
+        try:
+            print("Uploading file...")
+            response = insert_request.next_chunk()
+            if response is not None:
+                if 'id' in response:
+                    print(
+                        f"Video id '{response['id']}' was successfully uploaded.")
+                else:
+                    sys.exit(
+                        f"The upload failed with an unexpected response: {response}")
+        except HttpError as http_error:
+            if http_error.resp.status in RETRIABLE_STATUS_CODES:
+                error = f"A retriable HTTP error {http_error.resp.status} occurred:\n{http_error.content}" % (
+                    http_error.resp.status)
+            else:
+                raise
+        except RETRIABLE_EXCEPTIONS as error:
+            error = f"A retriable error occurred: {error}"
+
+        if error is not None:
+            print(error)
+            retry += 1
+            if retry > MAX_RETRIES:
+                sys.exit("No longer attempting to retry.")
+
+            max_sleep = 2 ** retry
+            sleep_seconds = random.random() * max_sleep
+            print(f"Sleeping {sleep_seconds} seconds and then retrying...")
+            time.sleep(sleep_seconds)
+
+
 def main():
     """
     The main entrypoint
     """
+    # securedata.writeFile("client-secrets.json", ".",
+    #                      str(securedata.getItem("reddit", "google-oath")))
+
     create_video()
+
+    argparser.add_argument("--file", default="output.mp4",
+                           help="Video file to upload")
+    argparser.add_argument("--title", help="Video title", default=VIDEO_TITLE)
+    argparser.add_argument("--description", help="Video description",
+                           default="I found these cool pictures on Reddit and wanted to share them with you all! Make sure to like and subscribe for the latest updates.")
+    argparser.add_argument("--category", default="22",
+                           help="Numeric video category. " +
+                           "See https://techpostplus.com/youtube-video-categories-list-faqs-and-solutions/")
+    argparser.add_argument("--keywords", help="Video keywords, comma separated",
+                           default="reddit, oldschoolcool, nostalgia")
+    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
+                           default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
+    args = argparser.parse_args()
+
+    if not os.path.exists(args.file):
+        sys.exit("Please specify a valid file using the --file= parameter.")
+
+    youtube = get_authenticated_service(args)
+
+    try:
+        initialize_upload(youtube, args)
+    except HttpError as error:
+        print(f"An HTTP error {error.resp.status} occurred:\n{error.content}")
 
 
 main()
